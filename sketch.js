b7b4d8c2b0f7de59f2f73abf4843928effd100a7
Moazzam Moriani <mozmoriani@gmail.com>
2022-01-23 20:42:18 +0500

    Introduced a Ball that Reacts to the Players Paddles I copy-pasted the ball constructor from my bouncy ball project and then made some adjustment to it. I created a predicate method that checks if the ball has come into contact with one of the paddles.

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ function setup() {
     createCanvas(1400,700);
     leftPlayer = new Paddle(true);
     rightPlayer = new Paddle(false);
+    ball = new Ball(20, [255,255,255], leftPlayer, rightPlayer);
 }
 
 
@@ -10,6 +11,7 @@ function draw() {
     background(0);
     leftPlayer.draw();
     rightPlayer.draw();
+    ball.draw();
 }
 
 
@@ -29,7 +31,8 @@ function Paddle(left) {
     this.paddleHeight = 90;
     this.paddleWidth = 20;
     this.yPos = height / 2;
-    centreDist = this.paddleHeight / 2;
+    this.centerHeightDist = this.paddleHeight / 2;
+    this.centerWidthDist = this.paddleWidth / 2;
 
     this.draw = () => {
         rectMode(CENTER);
@@ -40,12 +43,12 @@ function Paddle(left) {
     };
 
     this.paddleWithinLowerboundP = () => {
-        return dist(this.xPos, this.yPos, this.xPos, 0) > centreDist + 20;
+        return dist(this.xPos, this.yPos, this.xPos, 0) > this.centerHeightDist + 20;
         
     }
 
     this.paddleWithinUpperboundP = () => {
-        return dist(this.xPos, this.yPos, this.xPos, height) > centreDist + 20; 
+        return dist(this.xPos, this.yPos, this.xPos, height) > this.centerHeightDist + 20; 
     }
 
     this.updateYPos = () => {
@@ -61,6 +64,90 @@ function Paddle(left) {
     };
    
 }
+
+function Ball(diameter, colorArr, paddle1, paddle2) {
+	this.x = width / 2;
+	this.y = height / 2 - 100;
+	this.velocityX = -5;
+	this.velocityY = 1.2;
+	this.diameterX = diameter;
+	this.diameterY = diameter;
+	this.radius = diameter / 2;
+    this.ballColor = colorArr;
+
+	this.draw = () => {
+		rectMode(CENTER);
+        noStroke();
+		fill(this.ballColor[0], this.ballColor[1], this.ballColor[2]);
+        noStroke();
+		rect(this.x, this.y, this.diameterX, this.diameterY);
+		this.x += this.velocityX;
+		this.y += this.velocityY;
+
+        if (this.hitVertWallp() || this.hitPaddlep()) {
+			this.vertBounce();
+		} 
+
+		if (this.hitHorzWallp()) {
+			this.horzBounce();
+		}
+
+	};
+
+	this.hitVertWallp = () => {
+		return dist(this.x, this.y, 0, this.y) <= this.radius || dist(this.x, this.y, width, this.y) <= this.radius;
+	};
+
+	this.hitHorzWallp = () => {
+		return dist(this.x, this.y, this.x, 0) <= this.radius || dist(this.x, this.y, this.x, height) <= this.radius;
+	};
+
+	this.vertBounce = () => {
+		this.velocityX = -this.velocityX; 
+	};
+	this.horzBounce = () => {
+
+		this.velocityY = -this.velocityY;
+	};
+
+    this.getSpeed = () => {
+        return sqrt(this.velocityX**2 + this.velocityY**2);
+    };
+
+    /*this.drawTail = () => {
+
+    };*/
+
+    this.hitPaddlep = () => {
+
+        ballY = this.y;
+        function withinYRangep(low, high) {
+            return ballY < high && ballY > low;
+        }
+
+        paddle1Surface = paddle1.xPos + paddle1.centerWidthDist;
+        paddle1Top = paddle1.yPos - paddle1.centerHeightDist;
+        paddle1Bottom = paddle1.yPos + paddle1.centerHeightDist;
+
+        p1wr = withinYRangep(paddle1Top, paddle1Bottom);
+        if (this.x <= paddle1Surface && withinYRangep(paddle1Top, paddle1Bottom)) {
+            return true; 
+        }
+
+
+        paddle2Surface = paddle2.xPos - paddle2.centerWidthDist;
+        paddle2Top = paddle2.yPos - paddle2.centerHeightDist;
+        paddle2Bottom = paddle2.yPos + paddle2.centerHeightDist;
+
+        if (this.x >= paddle2Surface && withinYRangep(paddle2Top, paddle2Bottom)) {
+            return true; 
+        }
+
+        return false;
+    }
+}
+
+
 
 function keyisdown() {
     currentlypressedkeycode = keycode;
